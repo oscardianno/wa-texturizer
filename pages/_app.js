8000c66e8d940db860396a80d8c74af8eb21d8f6
Oscar Mora <oscare.ramirez@alumnos.udg.mx>
2022-06-13 16:42:36 -0500

    Add global stylesheet + dark background

---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+import "../styles/app.css"
+
+// This default export is required in a new `pages/_app.js` file.
+export default function MyApp({ Component, pageProps }) {
+    return <Component {...pageProps} />
+}
+
+// These styles (styles.css) will apply to all pages and components in your application. 
